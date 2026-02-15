a3f443872cf052dcfcb45e31e8a275aca72ca08e
SlytherinKid <mr.shubhamkr2000@gmail.com>
2026-02-15 15:16:21 +0530

    added UI of hero section

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import Navbar from "components/Navbar";
 import type { Route } from "./+types/home";
+import { ArrowRight, Layers } from "lucide-react";
+import Button from "components/ui/Button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,6 +14,36 @@ export default function Home() {
   return (
     <div className="home">
       <Navbar />
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"></div>
+          </div>
+            <p>Introducing Plotly 1.0</p>
+        </div>
+        <h1>Build beautiful spaces at the speed of thought with Plotly</h1>
+        <p className="subtitle">
+          Plotly is an AI-first design environment that helps you visualize, render, and ship 
+          architectural projects faster than ever.
+        </p>
+        <div className="actions">
+          <a href="#upload" className="cta">Start Building <ArrowRight className="icon" /> </a>
+          <Button variant="outline" size="lg" className="demo">Watch Demo</Button>
+        </div>
+        <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon"/>
+              </div>
+              <h3>Upload you floor plan</h3>
+              <p>Supports JPG, PNG, formats up to 10MB</p>
+            </div>
+            <p>Upload Images</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
